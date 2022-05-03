98db919a671295f3346f4852315312da905a50ae
Emil Mohammed <emohammed@prodigytel.com>
2022-05-03 17:28:11 -0400

    Began section parsing and formatting implementation

--- models/Section.js
+++ models/Section.js
@@ -1,10 +1,24 @@
-export class Section {
-    constructor (header){
-        this.header = header;
-        this.states = []
+class Section {
+    constructor (){
+        this.descriptionLines = [];
+        this.firstState = -1; 
+    }
+
+    parseDescriptionLine(line){
+        const data = line.match(/\*\s{2}(.+)\r?\n?/)
+        this.descriptionLines.push(data[1])
     }
 
     format() {
-        return `****************************************\n*  ${this.header}\n\n****************************************`
+        let text = "";
+        text = text.concat("****************************************");
+        text = text.concat('\n');
+        this.descriptionLines.forEach(element => {
+            text = text.concat(`*  ${element}\n`);
+        });
+        text = text.concat("****************************************");
+        return text;
     }
 }
+
+module.exports = Section;
---
+++ tests/Section.test.js
@@ -0,0 +1,56 @@
+const Section = require('../models/Section.js');
+
+test('parse description line single word', () => {
+    const input = "*  stuff";
+    const output = "stuff";
+    const section = new Section();
+    section.parseDescriptionLine(input);
+    expect(section.descriptionLines.length).toBe(1);
+    expect(section.descriptionLines[0]).toBe(output);
+});
+
+test('parse description line multiple words', () => {
+    const input = "*  lots of stuff";
+    const output = "lots of stuff";
+    const section = new Section();
+    section.parseDescriptionLine(input);
+    expect(section.descriptionLines.length).toBe(1);
+    expect(section.descriptionLines[0]).toBe(output);
+});
+
+test('parse description line words, numbers, and symbols', () => {
+    const input = "*  do 3 (three) things";
+    const output = "do 3 (three) things";
+    const section = new Section();
+    section.parseDescriptionLine(input);
+    expect(section.descriptionLines.length).toBe(1);
+    expect(section.descriptionLines[0]).toBe(output);
+});
+
+test('parse description line multiple', () => {
+    const input0 = "*  this is line number 1";
+    const input1 = "*  this is line number 2";
+    const output = ["this is line number 1", "this is line number 2"];
+    const section = new Section();
+    section.parseDescriptionLine(input0);
+    section.parseDescriptionLine(input1);
+    expect(section.descriptionLines.length).toBe(2);
+    expect(section.descriptionLines).toStrictEqual(output);
+});
+
+test('format single line description', () => {
+    const input = "this is line number 1";
+    const output = `****************************************\n*  ${input}\n****************************************`;
+    const section = new Section();
+    section.descriptionLines = [input];
+    expect(section.format()).toBe(output);
+});
+
+test('format multiple line description', () => {
+    const input0 = "this is line number 1";
+    const input1 = "this is line number 2";
+    const output = `****************************************\n*  ${input0}\n*  ${input1}\n****************************************`;
+    const section = new Section();
+    section.descriptionLines = [input0, input1];
+    expect(section.format()).toBe(output);
+});
