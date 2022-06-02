c7a687d5a96e9ad279f276adddd23cd9294a7d4f
emilmoham <emilmoham@gmail.com>
2022-06-01 23:16:23 -0400

    moved section tests to typescript

--- src/models/__tests__/Section.test.ts
+++ src/models/__tests__/Section.test.ts
@@ -1,9 +1,11 @@
+import SwitchState from '../SwitchState';
 import Section from '../Section';
 
 test('parse description line single word', () => {
   const input = '*  stuff';
   const output = 'stuff';
-  const section = new Section();
+  const testState = new SwitchState('test', [], [], 'test');
+  const section = new Section(testState, []);
   section.parseDescriptionLine(input);
   expect(section.descriptionLines.length).toBe(1);
   expect(section.descriptionLines[0]).toBe(output);
@@ -12,7 +14,8 @@ test('parse description line single word', () => {
 test('parse description line multiple words', () => {
   const input = '*  lots of stuff';
   const output = 'lots of stuff';
-  const section = new Section();
+  const testState = new SwitchState('test', [], [], 'test');
+  const section = new Section(testState, []);
   section.parseDescriptionLine(input);
   expect(section.descriptionLines.length).toBe(1);
   expect(section.descriptionLines[0]).toBe(output);
@@ -21,7 +24,8 @@ test('parse description line multiple words', () => {
 test('parse description line words, numbers, and symbols', () => {
   const input = '*  do 3 (three) things';
   const output = 'do 3 (three) things';
-  const section = new Section();
+  const testState = new SwitchState('test', [], [], 'test');
+  const section = new Section(testState, []);
   section.parseDescriptionLine(input);
   expect(section.descriptionLines.length).toBe(1);
   expect(section.descriptionLines[0]).toBe(output);
@@ -31,7 +35,8 @@ test('parse description line multiple', () => {
   const input0 = '*  this is line number 1';
   const input1 = '*  this is line number 2';
   const output = ['this is line number 1', 'this is line number 2'];
-  const section = new Section();
+  const testState = new SwitchState('test', [], [], 'test');
+  const section = new Section(testState, []);
   section.parseDescriptionLine(input0);
   section.parseDescriptionLine(input1);
   expect(section.descriptionLines.length).toBe(2);
@@ -41,7 +46,8 @@ test('parse description line multiple', () => {
 test('format single line description', () => {
   const input = 'this is line number 1';
   const output = `****************************************\n*  ${input}\n\n****************************************`;
-  const section = new Section();
+  const testState = new SwitchState('test', [], [], 'test');
+  const section = new Section(testState, []);
   section.descriptionLines = [input];
   expect(section.format()).toBe(output);
 });
@@ -50,7 +56,8 @@ test('format multiple line description', () => {
   const input0 = 'this is line number 1';
   const input1 = 'this is line number 2';
   const output = `****************************************\n*  ${input0}\n*  ${input1}\n\n****************************************`;
-  const section = new Section();
+  const testState = new SwitchState('test', [], [], 'test');
+  const section = new Section(testState, []);
   section.descriptionLines = [input0, input1];
   expect(section.format()).toBe(output);
 });
