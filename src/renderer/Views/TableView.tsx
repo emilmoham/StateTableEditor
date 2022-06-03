9eec785969299b320af3c5169e28dab98165bba7
Emil Mohammed <emilmoham@gmail.com>
2022-06-03 16:10:44 -0400

    Now rendering app table as a table

--- src/renderer/Views/TableView.tsx
+++ src/renderer/Views/TableView.tsx
@@ -1,7 +1,8 @@
 import AppTable from 'renderer/Models/AppTable';
 import AppTableContext from 'renderer/AppFileContext';
-import { useEffect, useContext } from 'react';
-import { NavItem } from 'react-bootstrap';
+import { useContext } from 'react';
+import { Container, Table } from 'react-bootstrap';
+import SwitchState from 'renderer/Models/SwitchState';
 
 export default function TableView() {
   const tables: AppTable[] = useContext(AppTableContext);
@@ -9,16 +10,46 @@ export default function TableView() {
   function renderStates() {
     if (tables.length > 0) {
       const table = tables[0];
-      return (
-        <div>
-          {table.stateMap.map((item) => {
-            return <p key={item.name}>{item.format(0)}</p>;
-          })}
-        </div>
-      );
+      table.resolveAllStateReturnIds();
+      return table.stateMap.map((state) => {
+        const stateId = state.getStateId(table.stateMap);
+        return (
+          <tr key={stateId}>
+            <td>{stateId}</td>
+            <td>{state.name}</td>
+
+            {state.returnStateIds.map((id) => {
+              return <td>{id}</td>;
+            })}
+
+            {[
+              ...Array(SwitchState.MAX_STATES - state.returnStateIds.length),
+            ].map(() => {
+              return <td />;
+            })}
+            <td>{state.description}</td>
+          </tr>
+        );
+      });
     }
-    return <div>None</div>;
+    return <tr>None</tr>;
   }
 
-  return <div>{renderStates()}</div>;
+  return (
+    <Container fluid>
+      <Table>
+        <thead>
+          <tr>
+            <th>StateID</th>
+            <th>Name</th>
+            {[...Array(SwitchState.MAX_STATES).keys()].map((x) => (
+              <th>{x}</th>
+            ))}
+            <th>description</th>
+          </tr>
+        </thead>
+        <tbody>{renderStates()}</tbody>
+      </Table>
+    </Container>
+  );
 }
