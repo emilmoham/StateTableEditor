d27e2850305449f58265c3a27d597f74de784638
Emil Mohammed <emilmoham@gmail.com>
2022-06-08 17:17:26 -0400

    added ability to update current text of cell

--- src/renderer/Components/EditableCell.tsx
+++ src/renderer/Components/EditableCell.tsx
@@ -1,8 +1,33 @@
 import { useState } from 'react';
 
 export default function EditableCell(props: { value: string }) {
-  // const [isInEditMode, setIsInEditMode] = useState(false);
   const { value } = props;
+  const [isInEditMode, setIsInEditMode] = useState(false);
+  const [currentValue, setCurrentValue] = useState(value);
 
-  return <td>{value}</td>;
+  if (isInEditMode)
+    return (
+      <td>
+        <input
+          className="edit"
+          type="text"
+          value={currentValue}
+          onChange={(event) => {
+            setCurrentValue(event.target.value);
+          }}
+          onBlur={() => {
+            setIsInEditMode(false);
+          }}
+        />
+      </td>
+    );
+  return (
+    <td
+      onDoubleClick={() => {
+        setIsInEditMode(true);
+      }}
+    >
+      {currentValue}
+    </td>
+  );
 }
